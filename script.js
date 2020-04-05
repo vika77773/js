5e447db6b5d98d4a4610674790153302c765a49b
vika77773 <63184394+vika77773@users.noreply.github.com>
2020-04-05 15:23:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,34 @@ console.log(myVar1);
 myVar1-155;
 console.log('возраст:', myVar1);
 console.log(2==2);
+
+ money=24634;
+console.log(money);
+ income='фриланс';
+console.log(income);
+ addExpenses=' транспорт,еда,одежда,налоги,налоги,налоги ';
+console.log(addExpenses);
+ deposit='10000';
+ myVar1=true;
+myVar1=false;
+console.log(deposit);
+mission=1000000;
+console.log(mission);
+period=11;
+console.log(period);
+
+addExpenses='6'
+console.log(addExpenses);
+
+let budgetDay;
+console.log(budgetDay);
+
+budgetDay='доход за месяц/10000';
+console.log(budgetDay);
+
+
+
+
+
+
+
