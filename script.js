6e1ec20fac4aff6fe8b510f0c3f34cf1f42aec93
vika77773 <63184394+vika77773@users.noreply.github.com>
2020-04-05 12:59:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿// hfusdhfyhseufhshfudsuhf
+/*jsdhjahdfjhsfj*/
+console.log('text');
+
+let myVar1;
+
+myVar1-15;
+
+console.log(myVar1);
+
+console.log('возраст:',myVar1);
+
+let myName='Вика';
+let meVar2=true;
+
+myVar2=false;
+console.log(myName);
+console.log(myVar2);
+
+console.log(2==2);
